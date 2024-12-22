9524a416dc61c0ef9d6f066ce00bb640ea5d3efb
onlyhappiness <onlyhappiness96@gmail.com>
2024-12-23 00:28:30 +0900

    feat: login screen

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:restaurant_app/common/component/custom_text_form_field.dart';
+import 'package:restaurant_app/common/layout/default_layout.dart';
+import 'package:restaurant_app/user/view/login_screen.dart';
+
+void main() {
+  runApp(_App());
+}
+
+class _App extends StatelessWidget {
+  const _App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'NotoSans'
+      ),
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
---
+++ lib/user/view/login_screen.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:restaurant_app/common/component/custom_text_form_field.dart';
+import 'package:restaurant_app/common/const/colors.dart';
+import 'package:restaurant_app/common/layout/default_layout.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+        child: SingleChildScrollView(
+          keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+      child: SafeArea(
+        top: true,
+        bottom: false,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              _Title(),
+              const SizedBox(
+                height: 16.0,
+              ),
+              _SubTitle(),
+              Image.asset(
+                'asset/img/misc/logo.png',
+                width: MediaQuery.of(context).size.width / 3 * 2,
+              ),
+              const SizedBox(
+                height: 16.0,
+              ),
+              CustomTextFormField(
+                onChanged: (String value) {},
+                hintText: '이메일을 입력해주세요.',
+              ),
+              CustomTextFormField(
+                onChanged: (String value) {},
+                hintText: '비밀번호를 입력해주세요.',
+                obscureText: true,
+              ),
+              const SizedBox(
+                height: 16.0,
+              ),
+              ElevatedButton(
+                  onPressed: () {},
+                  style:
+                      ElevatedButton.styleFrom(foregroundColor: PRIMARY_COLOR),
+                  child: Text('로그인')),
+              TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(foregroundColor: Colors.black),
+                  child: Text('회원가입'))
+            ],
+          ),
+        ),
+      ),
+    ));
+  }
+}
+
+class _Title extends StatelessWidget {
+  const _Title({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      '환영합니다!',
+      style: TextStyle(
+          fontSize: 34, fontWeight: FontWeight.w500, color: Colors.black),
+    );
+  }
+}
+
+class _SubTitle extends StatelessWidget {
+  const _SubTitle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      '이메일과 비밀번호를 입력해서 로그인 해주세요!\n오늘도 성공적인 주문이 되길 :)',
+      style: TextStyle(fontSize: 16, color: BODY_TEXT_COLOR),
+    );
+  }
+}
